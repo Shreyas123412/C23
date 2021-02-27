0571cf2f8ea2d8b39cf8d2d6fae65156f79b86eb
Shreyas123412 <77199036+Shreyas123412@users.noreply.github.com>
2021-02-27 10:59:24 +0530

    Add files via upload

---
+++ Cs23/Ground.js
@@ -0,0 +1,19 @@
+class Ground {
+    constructor(X , y , width , height){
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(X , y, width, height, options);
+        World.add(world, this.body);
+        this.width = width;
+        this.height = height;
+    }
+
+    display() {
+    
+        var pos =this.body.position;
+        fill ("brown");
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+    }
+}
